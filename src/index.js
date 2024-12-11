a81c874ae542c2d62560ddb6e429b5e2e642bfb7
Alejandro Rubio <arubiodeveloper@gmail.com>
2024-12-11 01:07:00 -0500

    Remove other files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Overlay from "./Overlay"; // Import the new overlay component
-import Overlay2 from "./Overlay2"; // Import the new overlay component
 import App from "./App";
 
 const Root = () => (
